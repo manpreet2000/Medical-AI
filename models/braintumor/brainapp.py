d3842b605465c0276cbdb4326b420e61b1a1ba9c
manpreet2000 <manpreetsingh5772000@gmail.com>
2020-11-07 23:54:59 +0530

    All models working with base64 yay

--- models/braintumor/brainapp.py
+++ models/braintumor/brainapp.py
@@ -3,9 +3,9 @@ from flask import Flask, request, render_template,url_for,Blueprint
 from flask_cors import CORS, cross_origin
 import shutil
 import models.braintumor.src.predict as predict 
-#predict=__import__("models.brain tumor.src.predict")
-import cv2
-import matplotlib.pyplot as plt
+import base64
+import numpy as np
+from io import BytesIO
 #brainapp = Flask(__name__)
 brainapp=Blueprint("brainapp",__name__,template_folder="templates",static_folder="static")
 #CORS(brainapp)
@@ -19,26 +19,27 @@ def index():
         image_file=request.files["file"]
         #print(image_file)
         if image_file:
-            shutil.rmtree(upload_folder)
-            os.makedirs(upload_folder)
-            image_loc=os.path.join(upload_folder,image_file.filename)
-            image_file.save(image_loc)
-            image_name = os.path.basename(image_loc)
-            image_name = image_name.split('.')[0]
-            print(image_name)
-            print(image_loc)
+            # shutil.rmtree(upload_folder)
+            # os.makedirs(upload_folder)
+            # image_loc=os.path.join(upload_folder,image_file.filename)
+            # image_file.save(image_loc)
+            # image_name = os.path.basename(image_loc)
+            # image_name = image_name.split('.')[0]
+            # print(image_name)
+            # print(image_loc)
             
-            # plt.imshow(cv2.imread(image_loc))
-            # plt.show()
-            print(upload_folder+"/"+image_name+"NEW.png")
-            classifier=predict.predict_img(image_loc,image_name,upload_folder)
-            classifier.predict_image()
+            # # plt.imshow(cv2.imread(image_loc))
+            # # plt.show()
+            # print(upload_folder+"/"+image_name+"NEW.png")
+            npimg = np.fromstring(image_file.read(),np.uint8)
+            classifier=predict.predict_img(npimg)
+            uri=classifier.predict_image()
             # plt.imshow(pred,cmap='gray')
             # plt.show()
             
             # cv2.imwrite(upload_folder+"/"+image_name+"New.png",pred)
             # print(pred.shape)
-            return render_template('/btindex.html',image_loc=image_name+"NEW.jpg")
+            return render_template('/btindex.html',image_loc=uri)
     return render_template('/btindex.html',image_loc=None)
 
 
